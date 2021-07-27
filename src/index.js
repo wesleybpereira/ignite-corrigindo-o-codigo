70cd9d2049f681b7e24c2a9a9f5e6166d5781bc0
Wesley <wesleybpereira@gmail.com>
2021-07-27 20:24:09 -0300

    :sparkles: Desafio realizado

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body
+  const { title, url, techs } = request.body;
 
   const repository = {
     id: uuid(),
@@ -23,20 +23,36 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const likes = repositories[repositoryIndex].likes;
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+
+ /*  const A = {...repositories[repositoryIndex]};
+  const B = {...updatedRepository};
+
+  console.log('A', A)
+  console.log('B', B) */
 
   repositories[repositoryIndex] = repository;
 
@@ -46,9 +62,9 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +82,11 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  const repository = repositories[repositoryIndex];
 
-  return response.json('likes');
+  repository.likes++
+
+  return response.json(repository);
 });
 
 module.exports = app;
